9e19d36d58063c0274f6c8564c78d7895666cec2
Oladimeji <boladimeji834@gmail.com>
2026-06-13 10:03:40 +0100

    feat: add the file store class

---
+++ src/file_store.cpp
@@ -0,0 +1,132 @@
+#include "file_store.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+FileStore::FileStore(
+    const std::string& accounts_file,
+    const std::string& transactions_file
+) : accounts_file(accounts_file), transactions_file(transactions_file) {}
+
+void FileStore::saveAccount(const Account& account) {
+    std::ofstream file(accounts_file, std::ios::app);
+    if (!file.is_open()) {
+        std::cout << "encountered an error when opening file.\n";
+        return;
+    }
+    file << account.getAccountID() << ","
+        << account.getOwnerName() << ","
+        << account.getBalance() << ","
+        << account.type_to_string(account.getAccountType()) << "\n";
+    std::cout << "account: " << account.getAccountID() << " has been added successfully.";
+}
+
+std::vector<Account> FileStore::loadAccounts() {
+    std::ifstream file(accounts_file);
+
+    if (!file.is_open()) {
+        std::cout << "error when opening file.\n";
+        return {};
+    }
+
+    std::vector<Account> accounts;
+    // create a variable for every linw in the csv file
+    std::string line;
+    while (std::getline(file, line)) {
+        // convert the line to a string stream
+        std::stringstream ss(line);
+
+        std::string account_id, owner_name, balance, type;
+        std::getline(ss, account_id, ',');
+        std::getline(ss, owner_name, ',');
+        std::getline(ss, balance, ',');
+        std::getline(ss, type, ',');
+
+        // call the Account constructor 
+        Account account(account_id, owner_name, std::stod(balance), Account::string_to_type(type));
+        // push account to the accounts vector
+        accounts.push_back(account);
+    }
+
+    if (accounts.empty()) {
+        std::cout << "no registered accounts to load.\n";
+        return {};
+    }
+    return accounts;
+}
+
+void FileStore::saveTransaction(const Transaction& transaction) {
+    // open the file
+    std::ofstream file(transactions_file, std::ios::app);
+    if (!file.is_open()){
+        std::cout << "failed to open file.\n";
+        return;
+    }
+
+    file << transaction.getTransactionID() << ","
+        << transaction.getAccountID() << ","
+        << transaction.getAmount() << ","
+        << Transaction::type_to_string(transaction.getTransactionType()) << ","
+        << transaction.getTimestamp() << "\n";
+
+    std::cout << "transaction: " << transaction.getAccountID() << " has been saved successfully.";
+}
+
+std::vector<Transaction> FileStore::loadTransactions(const std::string& account_id) {
+    // open the file
+    std::ifstream file(transactions_file);
+    if (!file.is_open()) {
+        std::cout << "error encountered when opening file.\n";
+        return {};
+    }
+
+    std::vector<Transaction> transactions;
+    std::string line; // variable to store each line of the file
+
+    // loop through each line in the file
+    while (std::getline(file, line)) {
+        // convert each line to a string stream
+        std::stringstream ss(line);
+        // identifiers to store each field
+        std::string t_id, t_account_id, t_amount, t_type, t_timestamp;
+
+        std::getline(ss, t_id, ',');
+        std::getline(ss, t_account_id, ',');
+        std::getline(ss, t_amount, ',');
+        std::getline(ss, t_type, ',');
+        std::getline(ss, t_timestamp, ',');
+
+        // call the Transaction constructor
+        Transaction transaction(
+            t_id, 
+            t_account_id,
+            std::stod(t_amount),
+            Transaction::string_to_type(t_type),
+            t_timestamp
+        );
+        // add transaction to transactions vector
+        transactions.push_back(transaction);
+    }
+
+    // applying the filter
+    // auto it = std::find_if(transactions.begin(), transactions.end(), [&accound_id](const Transaction& t) {
+    //     return t.getAccountID() == accound_id;
+    // });
+
+    // if (it == transactions.end()) {
+    //     std::cout << "account id " << accound_id << " not found.\n";
+    //     return;
+    // } 
+
+    std::vector<Transaction> result;
+    for (auto transaction : transactions) {
+        if (transaction.getAccountID() == account_id) {
+            result.push_back(transaction);
+        }
+    }
+    if (result.empty()) {
+        std::cout << "no transactions exist with the account id: " << account_id << "\n";
+        return {};
+    }
+    return result;
+}
